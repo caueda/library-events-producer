a0deb0e8139c0d551bdec470d7de4d103f068db8
carlos.ueda <carlos.ueda@gmail.com>
2021-12-06 04:17:37 -0400

    First commit

---
+++ src/main/java/com/devueda/libraryeventsproducer/LibraryEventsProducerApplication.java
@@ -0,0 +1,13 @@
+package com.devueda.libraryeventsproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryEventsProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryEventsProducerApplication.class, args);
+    }
+
+}
